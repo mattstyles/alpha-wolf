fe91a849e88c550dc8ec2dd64977444d86127694
Matt Styles <matt@personalurban.com>
2019-03-09 11:56:24 +0000

    add home view selector

--- packages/app/src/views/home.jsx
+++ packages/app/src/views/home.jsx
@@ -1,27 +1,36 @@
 
-import { View, Button, FlexBox } from 'react-basic-kit'
+import { createStructuredSelector } from 'reselect'
+import { View, Button, FlexBox, Pane } from 'react-basic-kit'
 
 import { connect } from 'signals'
 import { push, routes } from 'core/routing'
 import { generateMap } from 'core/map/actions'
 import { Map } from 'components/map'
+import { getMap, getMapSize } from 'core/map/selectors'
+
+const viewSelector = createStructuredSelector({
+  map: getMap,
+  size: getMapSize
+})
 
 export const Home = connect(
-  state => state.map,
-  (map) => (
+  viewSelector,
+  ({ map, size }) => (
     <View isPadded>
       <h1>Alpha Wolf</h1>
-      <FlexBox width='200px'>
-        <Button mb={2} primary onClick={() => generateMap()}>
-          Generate Map
-        </Button>
-        <Button primary onClick={() => push({
-          route: routes.simulation
-        })}>Go to simulation</Button>
-      </FlexBox>
-      <FlexBox>
-        <Map data={map.data} size={map.size} />
-      </FlexBox>
+      <Pane split>
+        <FlexBox width='200px'>
+          <Button mb={2} primary onClick={() => generateMap()}>
+            Generate Map
+          </Button>
+          <Button primary onClick={() => push({
+            route: routes.simulation
+          })}>Go to simulation</Button>
+        </FlexBox>
+        <FlexBox px={2}>
+          <Map data={map} size={size} />
+        </FlexBox>
+      </Pane>
     </View>
   )
 )
